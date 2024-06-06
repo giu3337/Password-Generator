d5d1a065dbc29a3842f002b79d40d0ccd17c121b
Giuseppe Garcia-Cancela <giu3337@icloud.com>
2024-06-06 17:10:09 -0500

    update names

--- generator.js
+++ generator.js
@@ -1,38 +1,62 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+// App Name: Password Generator
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "."];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 let outPutOneEl = document.getElementById("outputOne-el");
 let outPutTwoEl = document.getElementById("outputTwo-el");
 let btnCopyOneEl = document.getElementById("BtncopyOne-el");
 let btnCopyTwoEl = document.getElementById("BtncopyTwo-el");
 
+
 // Function 2 Random Numbers*15
-
-
-
 
 
 function randomPass() {
-  let firstPass = [];
-  for (let i = 0; i < 15; i++) {
-    let randomIndex = Math.floor(Math.random() * characters.length);
-    firstPass.push(characters[randomIndex]);
-    let noCommasFirstPass = firstPass.join(""); 
-    outPutOneEl.textContent = noCommasFirstPass; 
-  }
+    let passwordLength = document.querySelector("#passwordLength-el").value;
+    let includeSymbols = document.getElementById("include-symbols-el").checked;
+    let includeNumbers = document.getElementById("include-numbers-el").checked;
+    
+    let firstPass = [];
+    let secondPass = [];
 
-  let secondPass = [];
-  for (let i = 0; i < 15; i++) {
-    let randomIndex = Math.floor(Math.random() * characters.length);
-    secondPass.push(characters[randomIndex]);
-    let noCommasSecondPass = secondPass.join(""); 
-    outPutTwoEl.textContent = noCommasSecondPass; 
-  }
-}
+    if (!includeSymbols && !includeNumbers) {
+        allChars = characters;
+    } else if (includeSymbols && !includeNumbers) {
+        allChars = symbols;
+    } else if (!includeSymbols && includeNumbers) {
+        allChars = numbers;
+    } else {
+        allChars = characters.concat(symbols).concat(numbers);
+    }
+  
+    for (let i = 0; i < passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * allChars.length);
+      firstPass.push(allChars[randomIndex]);
+    }
+    let noCommasFirstPass = firstPass.join("");
+    outPutOneEl.textContent = noCommasFirstPass;
 
-// Function 2 Random Numbers*15
+    for (let i = 0; i < passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * allChars.length);
+      secondPass.push(allChars[randomIndex]);
+    }
+    let noCommasSecondPass = secondPass.join("");
+    outPutTwoEl.textContent = noCommasSecondPass;
+  }
+  
+document.getElementById("passwordLength-el").value = 8;
+document.getElementById("include-symbols-el").checked = true;
+document.getElementById("include-numbers-el").checked = true;
+document.getElementById("include-symbols-el").addEventListener("change", randomPass);
+document.getElementById("include-numbers-el").addEventListener("change", randomPass);
+
+
 
 
 // On Clicks
+
 
 function copyOutputOne() {
     let range = document.createRange();
@@ -40,37 +64,29 @@ function copyOutputOne() {
     window.getSelection().addRange(range);
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
-  
     btnCopyOneEl.textContent = "Copied";
   
     setTimeout(function() {
       btnCopyOneEl.textContent = "Copy";
     }, 1000);
   }
-  
+
+
   function copyOutputTwo() {
     let range = document.createRange();
     range.selectNode(outPutTwoEl);
     window.getSelection().addRange(range);
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
-  
     btnCopyTwoEl.textContent = "Copied";
-  
+
     setTimeout(function() {
-      btnCopyTwoEl.textContent = "Copy";
+        btnCopyTwoEl.textContent = "Copy";
     }, 1000);
   }
 
-
-
-btnCopyOneEl.addEventListener("click", copyOutputOne);
-btnCopyTwoEl.addEventListener("click", copyOutputTwo);
-
-
-
-// On Clicks
-
+  btnCopyOneEl.addEventListener("click", copyOutputOne);
+  btnCopyTwoEl.addEventListener("click", copyOutputTwo);
 
 
 // Dark Mode
@@ -87,5 +103,11 @@ darkModeButton.addEventListener("click", function() {
   }
 });
 
-// Dark Mode
+
+
+
+
+
+
+
 
